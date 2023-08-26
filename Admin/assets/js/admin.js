3300ef5fa3e442e42a4c65d40023fdcd01b2374b
Haider Ali <haiderali98.ha61@gmail.com>
2023-08-26 22:47:45 +0500

    #2

--- Admin/assets/js/admin.js
+++ Admin/assets/js/admin.js
@@ -1,7 +1,15 @@
 const getAllRolesAPI = "https://localhost:44383/getAllRoles";
+const addUserApi = "https://localhost:44383/addUser";
+//variables
+const userForm = document.querySelector(".form-add-user");
+const role = document.querySelector(".form-Role > .form-select");
 
 document.addEventListener("DOMContentLoaded", () => {
   getAllRoles();
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    AddUser();
+  });
 });
 
 const getAllRoles = () => {
@@ -21,4 +29,31 @@ const getAllRoles = () => {
     .catch((error) => {
       console.log("error", error);
     });
+};
+const AddUser = () => {
+  userForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent form submission
+    const userData = {
+      firstName: userForm.firstName.value,
+      lastName: userForm.lastName.value,
+      email: userForm.email.value,
+      role: role.value,
+    };
+
+    fetch(addUserApi, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Set the Content-Type header
+      },
+      mode: "cors", // Add CORS mode
+      body: JSON.stringify(userData), // Convert userData to JSON format
+    })
+      .then((response) => {
+        console.log(response);
+        // Handle response here
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 };
