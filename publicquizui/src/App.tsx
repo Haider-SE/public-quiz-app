649c9915e42001574274fe3a91dac16db0c7ada1
Haider Ali <haiderali98.ha61@gmail.com>
2023-08-31 21:51:46 +0500

    Fron-End BaseLine

--- publicquizui/src/App.tsx
+++ publicquizui/src/App.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 import "./App.css";
-import AdminDashboard from "./Components/Admin/AdminDashboard";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DashboardContent } from "./Components/Admin/Dashboard";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import Users from "./Components/Admin/Users";
+import MainPage from "./Components/Admin/MainPage";
+const arrayOfApps = [
+  {
+    path: "/",
+    element: <MainPage />,
+    icon: <DashboardIcon />,
+    primary: "Dashboard",
+  },
+  {
+    path: "/users",
+    element: <Users />,
+    icon: <AssignmentIcon />,
+    primary: "Users",
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <AdminDashboard />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {arrayOfApps.map((i: any, idx: any) => (
+          <>
+            <Route
+              path={i.path}
+              element={
+                <DashboardContent arrayOfApps={arrayOfApps}>
+                  {i.element}
+                </DashboardContent>
+              }
+              key={idx}
+            />
+          </>
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
